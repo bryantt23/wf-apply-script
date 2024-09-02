ce647bbc2b262d111b1302653f488b50cf0768d5
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-03 02:10:58 +0800

    can open companies & close them

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 {
-    const elements = document.querySelectorAll('[data-test="StartupResult"]');
+    const delay = (time = 500) => {
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })
+    }
 
-    elements[0].querySelector('a').click()
+    const closeCompany = async () => {
+        const closeButton = document.querySelector('[data-test="closeButton"]')
+        await delay(3000)
+        console.log("🚀 ~ closeCompany ~ closeButton:", closeButton)
+        closeButton.click()
+    }
+
+    const start = async () => {
+        const companies = document.querySelectorAll('[data-test="StartupResult"]');
+        for (let i = 0; i < 3; i++) {
+            await showCompanyJobs(companies[i])
+        }
+    }
+
+    async function showCompanyJobs(element) {
+        const links = element.querySelectorAll("a")
+        console.log("🚀 ~ showCompanyJobs ~ links:", links)
+        links[0].click()
+        await closeCompany()
+    }
+
+    start()
 }
