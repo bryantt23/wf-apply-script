0cebed322a1ee5a138e70f6130ab682e8a6004f6
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-04 01:00:06 +0800

    can apply to jobs

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 {
-    let jobLinksElements = [];
     const delay = (time = 500) => {
         return new Promise(resolve => {
             setTimeout(resolve, time)
@@ -18,10 +17,9 @@
 
     const start = async () => {
         const companies = document.querySelectorAll('[data-test="StartupResult"]');
-        // for (let i = 0; i < 3; i++) {
-        //     await showCompanyJobs(companies[i])
-        // }
-        showCompanyJobs(companies[4])
+        for (let i = 0; i < 3; i++) {
+            await showCompanyJobs(companies[i])
+        }
     }
 
     async function showCompanyJobs(element) {
@@ -33,48 +31,74 @@
         await closeCompany()
     }
 
-    const applyForJob = async () => {
+    const clickOnJobLink = async (jobLinks) => {
+        if (jobLinks.length === 1) {
+            await clickApplyButton(jobLinks[0])
+        }
+        else {
+            const frontEndLinks = jobLinks.filter(link => link.hasAttribute('href') && link.getAttribute("href").includes("front"))
+            const fullStackLinks = jobLinks.filter(link => link.hasAttribute('href') && link.getAttribute("href").includes("full"))
+            const softwareLinks = jobLinks.filter(link => link.hasAttribute('href') && link.getAttribute("href").includes("software"))
 
+            if (frontEndLinks.length > 0) {
+                await clickApplyButton(frontEndLinks[0])
+            }
+            else if (fullStackLinks.length > 0) {
+                await clickApplyButton(fullStackLinks[0])
+            }
+            else if (softwareLinks) {
+                await clickApplyButton(softwareLinks[0])
+            }
+            else {
+                await clickApplyButton(jobLinks[0])
+            }
+        }
+    }
+
+    const clickApplyButton = async (element) => {
+        const parentDiv = element.closest('div')
+        if (parentDiv) {
+            console.log(`🚀 ~ Parent Div of Link ${element}:`, parentDiv);
+
+            // Now find the sibling div that contains the buttons                
+            const siblingDiv = parentDiv.nextElementSibling
+
+            if (siblingDiv) {
+                console.log("🚀 ~ clickApplyButton ~ siblingDiv:", siblingDiv)
+                const buttons = siblingDiv.querySelectorAll("button")
+                console.log("🚀 ~ clickApplyButton ~ buttons:", buttons)
+                buttons[1].click()
+                await delay(3000); // Give it 1 second to load
+
+                let clickedApply = false
+                const allButtons = document.querySelectorAll('button')
+                for (const button of allButtons) {
+                    if (button.textContent.trim() === 'Send application') {
+                        console.log("🚀 ~ clickApplyButton ~ Sending application...");
+                        button.click()
+                        clickedApply = true
+                        break
+                    }
+                }
+                if (!clickedApply) {
+                    console.log("🚀 ~ clickApplyButton ~ Could not apply...");
+                }
+            } else {
+                console.log("Sibling div not found.");
+            }
+
+        }
+
+    }
+
+    const applyForJob = async () => {
         await delay(1000)
         const modal = document.querySelector('.ReactModal__Content ')
         await delay(1000)
-        console.log("🚀 ~ applyForJob ~ modal:", modal)
         const links = modal.querySelectorAll('a')
-        console.log("🚀 ~ applyForJob ~ links:", links)
-
-
         const jobLinks = Array.from(links).filter(link => link.hasAttribute('href') && link.getAttribute("href").startsWith("/jobs"))
-        console.log("🚀 ~ applyForJob ~ jobLinks:", jobLinks)
-        jobLinksElements = jobLinks
-
-        // Loop through the <a> tags and print their text content
-        for (let i = 0; i < links.length; i++) {
-            const linkText = links[i].textContent.trim()
-            console.log(`Link ${i + 1}: ${linkText}`);
-            const parentDiv = links[i].closest('div')
-            if (parentDiv) {
-                console.log(`🚀 ~ Parent Div of Link ${i + 1}:`, parentDiv);
-
-                // Now find the sibling div that contains the buttons                
-                const siblingDiv = parentDiv.nextElementSibling
-
-                if (siblingDiv) {
-                    console.log(`🚀 ~ Sibling Div of Link ${i + 1}:`, siblingDiv);
-
-                    const buttons = siblingDiv.querySelectorAll("button")
-
-                    if (Array.from(buttons).some(button => button.textContent === 'Apply')) {
-                        console.log(`${linkText} is a job application`)
-                    }
-                } else {
-                    console.log("Sibling div not found.");
-                }
-
-            }
-        }
-        // console.log("🚀 ~ applyForJob ~ links:", links)
-
-        debugger
+        // debugger
+        await clickOnJobLink(jobLinks)
     }
 
     start()
